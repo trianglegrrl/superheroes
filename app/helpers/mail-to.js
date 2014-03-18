ab8715578b92952affc9abcd90389c97154eea59
Alaina Hardie <alaina@precisionnutrition.com>
2014-03-18 12:13:04 -0400

    Fix mail-to helper warning in grunt's linting

--- app/helpers/mail-to.js
+++ app/helpers/mail-to.js
@@ -1,6 +1,6 @@
 export default Ember.Handlebars.makeBoundHelper(function (emailAddress, label) {
   emailAddress = Em.Handlebars.Utils.escapeExpression(emailAddress);
-  label = (arguments.length == 2) ? emailAddress : Em.Handlebars.Utils.escapeExpression(label);
+  label = (arguments.length === 2) ? emailAddress : Em.Handlebars.Utils.escapeExpression(label);
 
   var link = '<a href="mailto:' + emailAddress + '">' + label + '</a>';
   return new Em.Handlebars.SafeString(link);
